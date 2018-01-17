1a0eb502d51c84bb89638a38ad3b9358d0b4252a
veatrice185 <35174506+veatrice185@users.noreply.github.com>
2018-01-17 23:01:52 +0900

    Atcoder Beginner Contest 069

---
+++ OK結果ファイル/Atcoder Beginner Contest 069/A - K-City.c
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<math.h>
+using namespace std;
+ 
+ 
+int main()
+{
+	int n, m;
+	int total;
+	cin >> n >> m;
+ 
+	total = (n - 1)*(m - 1);
+ 
+	cout << total;
+	return 0;
+ 
+}
